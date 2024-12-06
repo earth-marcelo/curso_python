c4666bbb30d4fcdec1c7d98f4b6962becbed72e3
Marcelo Carvalho <earth.marcelo@@mail.com>
2024-12-06 19:48:18 -0400

    Primeiro commit

---
+++ aula001.py
@@ -0,0 +1,15 @@
+"""
+Docstring
+E escrever o que eu quiser
+
+"""
+
+# Permite escrever um comentario
+print(123) # Na frente
+# Abaixo
+
+'''
+Docstring
+E escrever o que eu quiser
+
+'''
---
+++ aula003.py
@@ -0,0 +1,26 @@
+"""
+Python = Linguagem de programacao
+Tipo de tipagem = Dinamica / Forte
+str -> string -> texto
+Strings sao textos que estao dentro de aspas
+"""
+print(1234)
+
+# aspas simples
+print('Marcelo Carvalho')
+print('"Marcelo Carvalho"')
+
+# Aspas duplas
+print("Marcelo Carvalho")
+print("'Marcelo Carvalho'")
+
+
+# Escape
+print("Marcelo \"Carvalho\"")
+
+# r 
+print(r"Marcelo \"Carvalho\"")
+
+
+
+
