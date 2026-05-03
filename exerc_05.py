ae861b209795d94d33c8eda2ef6fa1829493fc2d
Marcelo Carvalho <earth.marcelo@gmail.com>
2026-05-03 13:11:47 -0400

    exercicios iniciais

---
+++ exerc_05.py
@@ -0,0 +1,4 @@
+anomalias = [-45.2, -38.7, -52.1, -41.3, -48.9, -35.6, -50.2]
+
+for valor in anomalias:
+    print(valor)
---
+++ exerc_06.py
@@ -0,0 +1,5 @@
+nomes_estacoes = ["est_01", "est_02", "est_03", "est_04", "est_05", "est_06", "est_07"]
+anomalias = [-45.2, -38.7, -52.1, -41.3, -48.9, -35.6, -50.2]
+
+#for i in range(len(anomalias)):
+ #   print(nomes_estacoes[i], "->", anomalias[i], "mGal")
