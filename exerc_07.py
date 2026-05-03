923781a03c527dd0d4d5b8f4932f02df572fdce0
Marcelo Carvalho <earth.marcelo@gmail.com>
2026-05-03 14:11:48 -0400

    salvando os exercicios 6 a 9

--- exerc_07.py
+++ exerc_07.py
@@ -2,4 +2,4 @@ station_names = ["est_01", "est_02", "est_03", "est_04", "est_05", "est_06", "es
 bouguer = [-45.2, -38.7, -52.1, -41.3, -48.9, -35.6, -50.2]
 
 for i in range(len(bouguer)):
-   print(station_names[i], "->", bouguer[i], "mGal")
+    print(station_names[i], "->", bouguer[i], "mGal")
--- exerc_08.py
+++ exerc_08.py
@@ -1,5 +1,5 @@
 station_names = ["est_01", "est_02", "est_03", "est_04", "est_05", "est_06", "est_07"]
 bouguer = [-45.2, -38.7, -52.1, -41.3, -48.9, -35.6, -50.2]
 
-for i in range(len(bouguer)):
-   print(station_names[i], "->", bouguer[i], "mGal")
+for name, value in zip(station_names, bouguer):
+    print(name, "->", value, "mGal")
