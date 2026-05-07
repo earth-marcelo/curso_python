cb2bbff6a96e3e9ddb6aca16777b62a2be41986c
Marcelo Carvalho <earth.marcelo@gmail.com>
2026-05-07 15:30:33 -0400

    envio dos exercicios 24 a 25

---
+++ exerc_25.py
@@ -0,0 +1,34 @@
+import pandas as pd
+            
+# Reading the files
+df_267 = pd.read_csv("bouguer_erg_267.txt",
+                     sep=r"\s+",
+                     header=None,
+                     names=["longitude", "latitude", "bouguer_267"])
+
+df_290 = pd.read_csv("bouguer_erg_290.txt",
+                     sep=r"\s+",
+                     header=None,
+                     names=["longitude", "latitude", "bouguer_290"])
+
+# reading topography
+df_topo = pd.read_csv("topo_erg.txt",
+                      sep=r"\s+",
+                      header=None,
+                      names=["longitude", "latitude", "elevation"])
+
+# Combining 
+df = pd.DataFrame({
+    "longitude"     : df_267["longitude"],
+    "latitude"      : df_267["latitude"],
+    "bouguer_267"   : df_267["bouguer_267"],
+    "bouguer_290"   : df_290["bouguer_290"],
+    "elevation"     : df_topo["elevation"]
+})
+
+# Filter: keep only ocean (elevation < -200m)
+df_ocean = df[df["elevation"] < -200].reset_index(drop=True)
+
+
+correlation = df_ocean['bouguer_267'].corr(df_ocean['bouguer_290'])
+print(correlation)
---
+++ exerc_26.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use("TkAgg")
+import seaborn as sns
+
+# Reading the files
+df_267 = pd.read_csv("bouguer_erg_267.txt",
+                     sep=r"\s+",
+                     header=None,
+                     names=["longitude", "latitude", "bouguer_267"])
+
+df_290 = pd.read_csv("bouguer_erg_290.txt",
+                     sep=r"\s+",
+                     header=None,
+                     names=["longitude", "latitude", "bouguer_290"])
+
+# reading topography
+df_topo = pd.read_csv("topo_erg.txt",
+                      sep=r"\s+",
+                      header=None,
+                      names=["longitude", "latitude", "elevation"])
+
+# Combining 
+df = pd.DataFrame({
+    "longitude"     : df_267["longitude"],
+    "latitude"      : df_267["latitude"],
+    "bouguer_267"   : df_267["bouguer_267"],
+    "bouguer_290"   : df_290["bouguer_290"],
+    "elevation"     : df_topo["elevation"]
+})
+
+# Filter: keep only ocean (elevation < -200m)
+df_ocean = df[df["elevation"] < -200].reset_index(drop=True)
+
+
+correlation = df_ocean[["longitude", 'latitude','bouguer_267', 'bouguer_290', 'elevation']].corr()
+#print(correlation)
+
+# # Correlation heatmap
+plt.figure(figsize=(8, 6))
+sns.heatmap(correlation,
+             annot=True,
+             cmap="RdBu_r",
+             fmt=".2f")
+plt.title("Correlation matrix")
+plt.savefig("Correlation matrix.png", dpi=300, bbox_inches="tight")
+plt.show()
